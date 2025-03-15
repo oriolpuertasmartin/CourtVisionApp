225bad0d24814ce778479c8db46b7b63d4d520a1
oriol_puertas <127403878+oriolpuertasmartin@users.noreply.github.com>
2025-03-15 11:43:11 +0100

    backend actualizado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:8081', // Permitir solo este origen, para permitir todos los origenes '*'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Headers permitidos
+    credentials: true, // Si necesitas enviar cookies o autenticación
+  });
+
   await app.listen(3001);
 }
 bootstrap();
